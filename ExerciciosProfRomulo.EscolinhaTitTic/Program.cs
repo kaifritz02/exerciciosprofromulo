e9b5d1672b7044273b35c6e2b54a76bfd18c6ae8
kaifritz02 <kairon.lima1@gmail.com>
2022-07-06 12:00:58 -0300

    First math exercise in C#

---
+++ ExerciciosProfRomulo.EscolinhaTitTic/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciciosProfRomulo.EscolinhaTicTic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string name;
+            Console.WriteLine("Hello student! Before we start please enter your name: ");
+            name = Console.ReadLine();
+            Console.WriteLine("Enter your first grade: ");
+            int gradeOne;
+            gradeOne = int.Parse(Console.ReadLine());
+            int gradeTwo;
+            gradeTwo = int.Parse(Console.ReadLine());
+            int gradeThree;
+            gradeThree = int.Parse(Console.ReadLine());
+            double result = gradeOne + gradeTwo + gradeThree/3;
+
+            if (result >= 7)
+            {
+                Console.BackgroundColor = ConsoleColor.DarkGreen;
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.WriteLine(name + " , your avarage grade is: " + result + ". So you got approved. Congratulations!");
+            }
+            else if (result >= 3 && result <= 7)
+            {
+                Console.BackgroundColor = ConsoleColor.DarkYellow;
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.WriteLine(name + ", your avarage grade is: " + result + ". So you need to do a recover. Please study more!");
+            }
+            else
+            {
+                Console.BackgroundColor = ConsoleColor.DarkRed;
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.WriteLine(name + ", your avarage grade is: " + result + ". So you failed. Please study more!");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ ExerciciosProfRomulo.MatematicaBasica/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciciosProfRomulo.MatematicaBasica
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Exercícios 1 - Crie na Solution um novo projeto com nome do ExerciciosProfRomulo.MatematicaBasica
+             * Execício 2 - Crie na Solution um novo projeto com o nome de ExerciciosProfRomulo.EscolinhaTicTac
+             * Exercício 3 - No projeto MatematicAbasica crie um Programa que calcule a soma de doismnumeros inteiros. Considere que o usuário vai digitar esses dois números.
+             * 
+             * 
+             * 
+             */
+            int numberOne;
+            int numberTwo;
+            int sum;
+
+            Console.WriteLine("Young man, enter two numbers:");
+            numberOne = int.Parse(Console.ReadLine());
+            numberTwo = int.Parse(Console.ReadLine());
+            sum = numberOne + numberTwo;
+            System.Console.WriteLine("The result of your sum is: " + sum);
+            Console.ReadKey();
+        }
+    }
+}
+    
